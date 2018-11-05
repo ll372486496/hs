fd45c54cefc6bdcc70d9cb0dc740ac3b40af6d05
ll372486496 <372486496@qq.com>
2018-11-05 08:29:10 +0800

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,21 @@
+//封装获取元素
+function $(id){
+    var ele;
+    ele=document.getElementById(id);
+    return ele;
+}
+
+//封装创建xhr
+function createXhr(){
+    var xhr=null;
+
+    if (window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+    } else {
+        xhr = new ActiveXObject('Microsoft XMLHttp');
+    }
+
+    return xhr;
+}
+
+//
