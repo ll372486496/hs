d059697826887b1034d214a3c846d350864923db
ll372486496 <372486496@qq.com>
2018-11-15 19:39:10 +0800

    设计师图标按钮控制

--- js/index.js
+++ js/index.js
@@ -8,3 +8,43 @@ window.onscroll=function(){
   topnav.style.display="none";
   
 }
+//取得上、下一张按钮
+var preBtns=document.getElementsByClassName('pre-btn'),
+    nextBtns=document.getElementsByClassName('next-btn');
+    
+//要移动的元素
+var designers=document.querySelectorAll('.designer-container');
+//console.log(preBtns);console.log(nextBtns);console.log(designers);
+//给按钮添加监听
+  var moved1=0;
+  preBtns[0].onclick=function(){
+   
+    if(moved1<2){
+      moved1++;//console.log(moved1);
+      designers[0].style.left=`-${moved1*180}px`;//左移一个图标
+    }
+  }
+  nextBtns[0].onclick=function(){
+    if(moved1>0){
+      moved1--;//console.log(moved1);
+      designers[0].style.left=`-${moved1*180}px`;//右移一个图标
+    }
+   
+  }
+  var moved2=0;
+  preBtns[1].onclick=function(){
+   
+    if(moved1<2){
+      moved1++;//console.log(moved1);
+      designers[1].style.left=`-${moved1*180}px`;
+    }
+  }
+  nextBtns[1].onclick=function(){
+    if(moved1>0){
+      moved1--;//console.log(moved1);
+      designers[1].style.left=`-${moved1*180}px`;
+    }
+   
+  }
+
+
