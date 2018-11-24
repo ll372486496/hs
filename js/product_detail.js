f05471e25b2777391d1e6e4229e38fe2b3afc33c
ll372486496 <372486496@qq.com>
2018-11-24 16:54:31 +0800

    详情图放大图效果

--- js/product_detail.js
+++ js/product_detail.js
@@ -6,7 +6,7 @@ for(var btn of btns){
 
     btn.onclick=function (){
     var btn=this;
-    console.log(btn);
+    
     var span=c_btn.getElementsByTagName('span')[0];
     if(btn.innerHTML==='+'){
       span.innerHTML++;
@@ -19,12 +19,14 @@ for(var btn of btns){
 //标签切换
 var as=document.querySelectorAll('#tab>li>[data-toggl=tab]');
 for(var a of as){
-  a.onclick=function(){
+  a.onclick=function(e){
+   e.preventDefault();
+   
    var a=this;
    var id=a.getAttribute('data-id');
    var div=document.querySelector(`#${id}`);
    var divs=document.querySelectorAll('.contents>div');
-   console.log(divs);
+   
    for(var d of divs){
     d.style.display='none';
     
@@ -37,17 +39,38 @@ for(var a of as){
 //详情图切换
 var img_list=document.getElementById('pd-img-list');
 var img_detail=document.getElementById('img-detail');
-var imgs_detail=img_detail.querySelector('img');
-var imgs=img_list.querySelectorAll('li img');
 
+var imgs=img_list.querySelectorAll('li img');
+var lgDiv=document.getElementById('lgDiv');
 for(var i=0;i<imgs.length;i++){
   
   imgs[i].onclick=function(){
-    var n=i;
+    for(var j=0;j<imgs.length;j++){
+      imgs[j].style.border=''
+    }
+    this.style.border='1px solid #000'
+    var md=this.getAttribute('data-md');
+    var lg=this.getAttribute('data-lg');
+    console.log(lgDiv);
+    console.log(lg);
+    lgDiv.style.backgroundImage=`url(${lg})`;
+    img_detail.style.backgroundImage=`url(${md})`;
     
-    this.style.border='1px solid #000';
-    imgs_detail.src=this.src;
     
+  }
+  img_detail.onmouseover=function(){
     
+    lgDiv.style.display='block';
+    
+  }
+  img_detail.onmousemove=function(e){
+    var msize=150;
+    var left=e.offsetX-msize/2;
+    var top=e.offsetY-msize/2;
+    lgDiv.style.backgroundPosition=
+        `-${2*left}px -${2*top}px`
+  }
+  img_detail.onmouseout=function(){
+    lgDiv.style.display='none';
   }
 }
