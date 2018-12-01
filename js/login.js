e9da656533044d798e0795b647443abbfdf88701
ll372486496 <372486496@qq.com>
2018-12-01 15:49:12 +0800

    登录页面方式切换

---
+++ js/login.js
@@ -0,0 +1,21 @@
+var as=document.querySelectorAll('#login .tabMenu>li>[data-toggle=tab]');
+/* console.log(as); */
+for(var a of as){
+ 
+ a.onclick=function(){
+  var a=this;
+  var id=a.getAttribute("data-id");
+  var ul=document.querySelector(id);
+  var uls=document.querySelectorAll("#login-content>ul");
+  var lis=document.querySelectorAll('#login>.tabMenu>li');
+  for(li of lis){
+    li.style.borderBottom='0'
+  }
+  for(u of uls){
+    u.style.display='none';
+  }
+  a.parentElement.style.borderBottom='2px solid transparent';
+  a.parentElement.style.borderColor='#00c8c8';
+  ul.style.display='block';
+ }
+}
