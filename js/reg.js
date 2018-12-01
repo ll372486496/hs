24b6434900160ea7649f9a5623de32c237ea69dd
ll372486496 <372486496@qq.com>
2018-12-01 14:08:45 +0800

    表单验证
    改用表单

--- js/reg.js
+++ js/reg.js
@@ -1,29 +1,15 @@
-//判断能否提交注册
-function reg() {
-  console.log('reg');
-  var span1 = $('ck-name-em').innerHTML, span2 = $("ck-pwd-em").innerHTML, span3 = $("ck-pwd-same").innerHTML;
-  console.log(span1 != '');
-  console.log(span2 != '');
-  console.log(span3 != '');
-  //判断填写是否有错
-  if (span1 != '' || span2 != '' || span3 != '') {
-    console.log('填写错误');
-  } else {
-    var xhr = new createXhr();
-    var name = $('login_name').value, pwd = $('login_pwd1').value;
-    //todo 用ajax提交到后台处理
-    console.log('可以注册');
-
-  }
-}
 //注册验证
 //1、获取名称、密码、验证密码元素，提示信息元素
 var username = document.getElementById('login_name');
 var upwd1 = document.getElementById('login_pwd1');
 var upwd2 = document.getElementById('login_pwd2');
-var upwdinfo = upwd2.nextElementSibling;
-var span = username.parentElement.children[1];
-
+var ckinfo = document.getElementsByClassName("ck-info");
+var upwdinfo = ckinfo[1];
+var span = ckinfo[0];
+var form = document.getElementById('reg-form');
+var emailreg = new RegExp(/^([a-zA-Z0-9._-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/);
+var phonereg = new RegExp(/^[1][3-9][0-9]{9}$/);
+var unok = false, pdok = false;
 
 //2、验证用户名
 //获得焦点时提示
@@ -34,16 +20,11 @@ username.onfocus = function () {
 }
 //验证用户名是否正确
 username.onblur = function () {
-  console.log('onblur');
   var username = this;
-  var emailreg = new RegExp(/^([a-zA-Z0-9._-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/);
-  var phonereg = new RegExp(/^[1][3,4,5,7,8][0-9]{9}$/);
-  /* var hid=document.getElementsByClassName('hid')[0]; */
-  //验证邮箱或手机号
   var txt = username.value;
 
   if (emailreg.test(txt) || phonereg.test(txt)) {
-    console.log(emailreg.test(txt) || phonereg.test(txt));
+    unok = true;
     span.innerHTML = '格式符合要求'
   } else {
 
@@ -54,11 +35,25 @@ username.onblur = function () {
 
 
 //3、验证密码
-upwd1.onblur=upwd2.onblur=function(){
-  if(upwd1.value==''||upwd2.value==''){
-    upwdinfo.innerHTML='密码不能为空';
-    upwdinfo.style.display='block';
-  }else{
-    upwdinfo.style.display='none';
+upwd1.onblur = upwd2.onblur = function () {
+  if (upwd1.value == upwd2.value && upwd1.value != '') {
+    pkok = true;
+    upwdinfo.style.display = 'none';
+  } else {
+    upwdinfo.innerHTML = '密码不一致';
+    upwdinfo.style.display = 'block';
+  }
+}
+
+//判断能否提交注册,通过<button type='submit'
+form.onsubmit = function (e) {
+  if (unok && pkok) {
+    var url=
+    ajax()
+    .then()
+    console.log('可以注册');
+  } else {
+    e.preventDefault();
+    console.log('不可以注册');
   }
 }
